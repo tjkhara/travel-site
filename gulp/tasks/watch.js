02530040d80f34945eb2242088c417d8644b381c
Tajeshwar Khara <tkhara@gmail.com>
2019-10-18 11:25:17 +0530

    Completed large hero block and intro content block

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+gulp.task('watch', function() {
+
+    browserSync.init({
+        server: {
+            baseDir: "app"
+        }
+    });
+
+  watch('./app/index.html', function() {
+      browserSync.reload();
+  });
+
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
+  });
+
+});
+
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream())
+        .on('error', function(errorInfo){
+            console.log(errorInfo.toString());
+            this.emit('end');
+        });
+ 
+});
+
